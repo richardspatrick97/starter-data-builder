ad4073ea96f32fbe43acb2ef57f61939fd447c80
ifeoluwanimie <onibokun_ifeoluwani@yahoo.com>
2024-08-16 10:42:18 -0400

    Updated pom file & module-info file  to pass Maven build.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,12 +7,10 @@ open module tinkar.sandbox {
     requires dev.ikm.tinkar.provider.executor;
     requires dev.ikm.tinkar.provider.search;
     requires dev.ikm.tinkar.coordinate;
-    requires transitive dev.ikm.tinkar.bindings;
-    requires transitive dev.ikm.tinkar.transformer.api;
-    requires transitive com.google.protobuf;
     requires org.eclipse.collections.api;
     requires java.logging;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.uuid;
     requires org.slf4j;
     requires org.eclipse.collections;
 
